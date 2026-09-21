d1cc06d4655f672ae83049b8a2a95f0b1c92290b
JavierE-Paz <javierpaz@ufm.edu>
2026-09-20 21:12:17 -0600

    respuesta api actualizada

--- comparador_carros.py
+++ comparador_carros.py
@@ -6,7 +6,7 @@ api_key = API_KEY_CARROS
 url = "https://api.api-ninjas.com/v1/cars"
 
 marca = input("Ingrese una marca: ").lower().strip()
-modelo = input("\nIngrese un modelo: ").lower().strip()
+modelo = input("Ingrese un modelo: ").lower().strip()
 
 
 headers = {
@@ -25,12 +25,16 @@ if respuesta.status_code == 200:
     datos = respuesta.json()
 
     for carro in datos:
-        print("Marca:", carro["make"])
-        print("Modelo:", carro["model"])
-        print("Año:", carro["year"])
-        print("Combustible:", carro["fuel_type"])
-        print("Transmisión:", carro["transmission"])
-        print("-" * 30)
+        for carro in datos:
+                print("Marca:", carro.get("make", "No disponible"))
+                print("Modelo:", carro.get("model", "No disponible"))
+                print("Año:", carro.get("year", "No disponible"))
+                print("Combustible:", carro.get("fuel_type", "No disponible"))
+                print("Transmisión:", carro.get("transmission", "No disponible"))
+                print("Sistema de tracción:", carro.get("drive", "No disponible"))
+                print("Cilindros:", carro.get("cylinders", "No disponible"))
+                print("Clase:", carro.get("class", "No disponible"))
+                print("-" * 30)
 
 else:
     print("Error:", respuesta.status_code)
